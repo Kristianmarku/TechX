43abde0315c1f7e830ecda84aebe7df07c1212b6
Kristian <kristianmarku2003@gmail.com>
2023-05-20 19:23:16 +0200

    Fixed cart closing bug

--- public/js/cart.js
+++ public/js/cart.js
@@ -2,15 +2,27 @@ const cartIcon = document.getElementById("cart-icon-a");
 const cartSection = document.querySelector(".cart-a");
 
 cartIcon.addEventListener("click", () => {
-    anime({
-        targets: cartSection,
-        translateX: ["100%", "0%"],
-        duration: 300,
-        easing: "easeInOutQuad",
-        begin: () => {
-            cartSection.style.display = "block";
-        },
-    });
+    if (cartSection.style.display == "block") {
+        anime({
+            targets: cartSection,
+            translateX: ["0%", "100%"],
+            duration: 300,
+            easing: "easeInOutQuad",
+            complete: () => {
+                cartSection.style.display = "none";
+            },
+        });
+    } else {
+        anime({
+            targets: cartSection,
+            translateX: ["100%", "0%"],
+            duration: 300,
+            easing: "easeInOutQuad",
+            begin: () => {
+                cartSection.style.display = "block";
+            },
+        });
+    }
 });
 
 const closeButton = document.getElementById("close-cart-a");
